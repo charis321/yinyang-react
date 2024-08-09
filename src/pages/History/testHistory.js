421bd1b467d7803f915d478c55351ab97ae8b55f
charis321 <z411327@gmail.com>
2024-08-09 17:44:51 +0800

    update user history 2024_08_09

--- src/pages/History/testHistory.js
+++ src/pages/History/testHistory.js
@@ -26,5 +26,61 @@ export const testHistory =  [
       "createTime": "2024-08-09T09:28:15",
       "title": "震之既濟",
       "yaosList": "786968"
-  }
+  },
+  {
+    "historyId": 44,
+    "userId": 6,
+    "createTime": "2024-08-09T09:20:00",
+    "title": "晉之既濟",
+    "yaosList": "686969"
+},
+{
+    "historyId": 46,
+    "userId": 6,
+    "createTime": "2024-08-09T09:28:07",
+    "title": "屯之損",
+    "yaosList": "768896"
+},
+{
+    "historyId": 47,
+    "userId": 6,
+    "createTime": "2024-08-09T09:28:11",
+    "title": "蠱之離",
+    "yaosList": "697687"
+},
+{
+    "historyId": 48,
+    "userId": 6,
+    "createTime": "2024-08-09T09:28:15",
+    "title": "震之既濟",
+    "yaosList": "786968"
+},
+{
+  "historyId": 44,
+  "userId": 6,
+  "createTime": "2024-08-09T09:20:00",
+  "title": "晉之既濟",
+  "yaosList": "686969"
+},
+{
+  "historyId": 46,
+  "userId": 6,
+  "createTime": "2024-08-09T09:28:07",
+  "title": "屯之損",
+  "yaosList": "768896"
+},
+{
+  "historyId": 47,
+  "userId": 6,
+  "createTime": "2024-08-09T09:28:11",
+  "title": "蠱之離",
+  "yaosList": "697687"
+},
+{
+  "historyId": 48,
+  "userId": 6,
+  "createTime": "2024-08-09T09:28:15",
+  "title": "震之既濟",
+  "yaosList": "786968"
+},
 ]
